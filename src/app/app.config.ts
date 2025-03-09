ac091f3f0a9f6339c709fdafbc388bf19d3c67a4
NesrineYAH <nesrineyahoum@gmail.com>
2025-03-10 00:37:21 +0100

    display tasks list

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angul
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+import { provideHttpClient } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),  provideClientHydration(withEventReplay()), provideRouter(routes),
-    { provide: LOCALE_ID, useValue: 'fr-FR' }
+  providers: [
+    provideHttpClient() // <- Ajoute ceci ici
   ]
 
 
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/*import { Injectable } from '@angular/core';
 import { Task } from '../models/Task';
 import { Observable, Subject } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -52,4 +52,66 @@ toggleAddTask(): void {
       return this.http.post<Task>(this.apiUrl, task, httpOptions);
     }
 }
+*/
 
+import { Injectable } from '@angular/core';
+import { Task } from '../models/Task';
+import { Observable, Subject, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+//import { AuthService } from './auth.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private showAddTask: boolean = false;
+  private subject = new Subject<boolean>();
+  private apiUrl = 'http://localhost:3000/tasks';
+
+  private tasks: Task[] = [
+    { id: 1, title: 'Task 1', status: 'todo', reminder: false },
+    { id: 2, title: 'Task 2', status: 'in_progress', reminder: true },
+    { id: 3, title: 'Task 3', status: 'done', reminder: false }
+  ];
+
+  constructor(private http: HttpClient) {}
+  //constructor( private http: HttpClient,   private auth: AuthService)) {}
+
+  // Récupérer toutes les tâches
+  getTasks(): Observable<Task[]> {
+ return of([...this.tasks]);
+   //    return  this.http.get<Task[]>(this.apiUrl);
+  }
+
+  // Mettre à jour une tâche
+  updateTask(id: number, status?: 'todo' | 'in_progress' | 'done', reminder?: boolean): Observable<Task> {
+    const taskUpdate = { status, reminder, updatedAt: new Date() };
+    return this.http.put<Task>(`${this.apiUrl}/${id}`, taskUpdate, httpOptions);
+   }
+  // Supprimer une tâche
+  deleteTask(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  // Ajouter une tâche
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
+
+  // Toggle de l'ajout de tâche
+  toggleAddTask(): void {
+    this.showAddTask = !this.showAddTask;
+    this.subject.next(this.showAddTask);
+  }
+
+  getToggleState(): Observable<boolean> {
+    return this.subject.asObservable();
+  }
+}
