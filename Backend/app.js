6a3ffe38a6582c8578fc50bfd54378949f9763be
NesrineYAH <nesrineyahoum@gmail.com>
2025-06-04 23:56:59 +0200

    //

--- Backend/app.js
+++ Backend/app.js
@@ -14,6 +14,8 @@ app.use(
     credentials: true,
   })
 );
+app.use("/uploads", express.static("uploads"));
+
 /*
 app.get('/api/tasks/lists/projects/:id', (req, res) => {
   // retourne bien du JSON !
