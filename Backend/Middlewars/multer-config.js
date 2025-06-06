3e2073b41e1ac4e74fdc97a5117c3236bb657da7
NesrineYAH <nesrineyahoum@gmail.com>
2025-06-07 01:07:18 +0200

    Update project component

--- Backend/Middlewars/multer-config.js
+++ Backend/Middlewars/multer-config.js
@@ -4,7 +4,7 @@ const MIME_TYPES = {
   "image/jpg": "jpg",
   "image/jpeg": "jpg",
   "image/png": "png",
-  "file/pdf": "pdf",
+  "application/pdf": "pdf",
 };
 
 const storage = multer.diskStorage({
@@ -12,10 +12,18 @@ const storage = multer.diskStorage({
     callback(null, "uploads/");
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
+    const name = file.originalname.split(" ").join("_").split(".")[0]; // évite de doubler l'extension
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
+    callback(null, name + "_" + Date.now() + "." + extension);
   },
 });
+
+const fileFilter = (req, file, callback) => {
+  if (MIME_TYPES[file.mimetype]) {
+    callback(null, true);
+  } else {
+    callback(new Error("Type de fichier non autorisé"), false);
+  }
+};
 
 module.exports = multer({ storage: storage }).single("file");
